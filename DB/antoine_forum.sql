fbe303240bd233f33fa87d02294db48bab2d6798
AHdev67 <antoinehelfer.contact@gmail.com>
2024-03-20 16:42:34 +0100

    current export of forum database

---
+++ DB/antoine_forum.sql
@@ -0,0 +1,88 @@
+-- --------------------------------------------------------
+-- Hôte:                         127.0.0.1
+-- Version du serveur:           8.0.30 - MySQL Community Server - GPL
+-- SE du serveur:                Win64
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Listage de la structure de la base pour antoine_forum
+CREATE DATABASE IF NOT EXISTS `antoine_forum` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `antoine_forum`;
+
+-- Listage de la structure de table antoine_forum. category
+CREATE TABLE IF NOT EXISTS `category` (
+  `id_category` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_category`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table antoine_forum.category : ~0 rows (environ)
+INSERT INTO `category` (`id_category`, `name`) VALUES
+	(1, 'General'),
+	(2, 'Help needed'),
+	(3, 'Rate my setup'),
+	(4, 'Humor/Meme'),
+	(5, 'Troubleshooting');
+
+-- Listage de la structure de table antoine_forum. post
+CREATE TABLE IF NOT EXISTS `post` (
+  `id_post` int NOT NULL AUTO_INCREMENT,
+  `content` text NOT NULL,
+  `creationDate` datetime NOT NULL,
+  `user_id` int NOT NULL,
+  `topic_id` int NOT NULL,
+  PRIMARY KEY (`id_post`) USING BTREE,
+  KEY `user` (`user_id`) USING BTREE,
+  KEY `topic` (`topic_id`) USING BTREE,
+  CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`),
+  CONSTRAINT `post_ibfk_2` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`id_topic`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table antoine_forum.post : ~0 rows (environ)
+
+-- Listage de la structure de table antoine_forum. topic
+CREATE TABLE IF NOT EXISTS `topic` (
+  `id_topic` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(150) NOT NULL,
+  `content` text NOT NULL,
+  `creationDate` datetime NOT NULL,
+  `category_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `closed` binary(50) DEFAULT NULL,
+  PRIMARY KEY (`id_topic`) USING BTREE,
+  KEY `category` (`category_id`) USING BTREE,
+  KEY `user` (`user_id`) USING BTREE,
+  CONSTRAINT `topic_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id_category`),
+  CONSTRAINT `topic_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table antoine_forum.topic : ~0 rows (environ)
+
+-- Listage de la structure de table antoine_forum. user
+CREATE TABLE IF NOT EXISTS `user` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `nickName` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `creationDate` datetime NOT NULL,
+  `bio` text,
+  PRIMARY KEY (`id_user`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table antoine_forum.user : ~0 rows (environ)
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
