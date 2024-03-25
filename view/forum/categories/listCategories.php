195d1d93bb7df64d86e6895118dd969e48bd70cf
AHdev67 <antoinehelfer.contact@gmail.com>
2024-03-25 11:55:43 +0100

    relocated files

---
+++ view/forum/categories/listCategories.php
@@ -0,0 +1,13 @@
+<?php
+    $categories = $result["data"]['categories']; 
+?>
+
+<h1>Categories list :</h1>
+
+<?php
+foreach($categories as $category ){ ?>
+    <p><a href="index.php?ctrl=forum&action=listTopicsByCategory&id=<?= $category->getId() ?>"><?= $category->getName() ?></a></p>
+<?php }
+
+
+  
