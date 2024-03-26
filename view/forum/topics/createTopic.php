9fbee4ebef5f19f441466cdf5d6906203340a2e7
AHdev67 <antoinehelfer.contact@gmail.com>
2024-03-26 16:59:25 +0100

    fixed submit and form action, changed checkboxes for radio buttons

--- view/forum/topics/createTopic.php
+++ view/forum/topics/createTopic.php
@@ -2,14 +2,14 @@
     $categories = $result["data"]['categories']; 
 ?>
 
-<form action="add" method="post">
+<form action="index.php?ctrl=topic&action=createTopic" method="post">
 
     <div>
         <span>Category : </span><br>
         <?php
         foreach($categories as $category) { ?>
             <p>
-                <input type="checkbox" value="<?= $category->getId() ?>" name="<?= "category".$category->getID() ?>" id="<?= $category->getID() ?>">
+                <input type="radio" value="<?= $category->getId() ?>" name="inputCategory" id="<?= $category->getID() ?>">
                 <label for="<?= $category->getID() ?>"><?= $category->getName() ?></label>
             </p>
         <?php } ?>
@@ -25,6 +25,6 @@
         <textarea name="inputContent" id="contentInput" cols="30" rows="10" placeholder="Object of your topic"></textarea>
     </div>
 
-    <input type="submit" name="submitTopic" id="topicSubmit" value="Post">
+    <input type="submit" name="submit" id="topicSubmit" value="Post">
 
 </form>
