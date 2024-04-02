547e05643f4b3ca3a3285558324f07df8a823291
AHdev67 <antoinehelfer.contact@gmail.com>
2024-04-02 17:00:56 +0200

    added methods to find user by email and username

--- model/managers/UserManager.php
+++ model/managers/UserManager.php
@@ -13,4 +13,31 @@ class UserManager extends Manager{
     public function __construct(){
         parent::connect();
     }
+
+    public function findUserByEmail($email) {
+
+        $sql = "SELECT * 
+                FROM ".$this->tableName." t 
+                WHERE t.email = :email";
+       
+        // la requête renvoie plusieurs enregistrements --> getMultipleResults
+        return  $this->getMultipleResults(
+            DAO::select($sql, ['email' => $email]), 
+            $this->className
+        );
+    }
+
+    public function findUserByUsername($username) {
+
+        $sql = "SELECT * 
+                FROM ".$this->tableName." t 
+                WHERE t.username = :username";
+       
+        // la requête renvoie plusieurs enregistrements --> getMultipleResults
+        return  $this->getMultipleResults(
+            DAO::select($sql, ['username' => $username]), 
+            $this->className
+        );
+    }
+
 }
