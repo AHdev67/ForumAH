789131daa5a773c00c2e1b10f8616a036748d1d7
AHdev67 <antoinehelfer.contact@gmail.com>
2024-04-02 17:00:28 +0200

    added success conditions and password hashing to register method

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -3,6 +3,7 @@ namespace Controller;
 
 use App\AbstractController;
 use App\ControllerInterface;
+use App\Session;
 use Model\Managers\UserManager;
 
 class SecurityController extends AbstractController{
@@ -25,24 +26,66 @@ class SecurityController extends AbstractController{
             $passwordCheck = filter_input(INPUT_POST,"inputPassword2", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         
             if($username && $email && $password) {
-                
-
-                $userData = [
+                if($userManager->findUserByEmail($email) != null){
+                    Session::addFlash("error", "E-mail adress already in use.");
+                    return [
+                        "view" => VIEW_DIR."security/register.php",
+                        "meta_description" => "User registration form"
+                    ];
+                }
+                else if ($userManager->findUserByUsername($username) != null){
+                    Session::addFlash("error", "Username already in use.");
+                    return [
+                        "view" => VIEW_DIR."security/register.php",
+                        "meta_description" => "User registration form"
+                    ];
+                }
+                else if($password != $passwordCheck){
+                    Session::addFlash("error", "Passwords don't match.");
+                    return [
+                        "view" => VIEW_DIR."security/register.php",
+                        "meta_description" => "User registration form"
+                    ];
+                }
+                else if(strlen($password) < 8){
+                    Session::addFlash("error", "Passwords is too short.");
+                    return [
+                        "view" => VIEW_DIR."security/register.php",
+                        "meta_description" => "User registration form"
+                    ];
+                }
+                else {
+                    $userData = [
                     "username" => $username,
                     "email" => $email,
-                    "password" => $password,
-                ];
+                    "password" => password_hash($password, PASSWORD_DEFAULT)
+                    ];
 
-                $userManager->add($userData);
+                    $userManager->add($userData);
 
-                return [
-                    "view" => VIEW_DIR."home.php",
-                    "meta_description" => "Page d'accueil du forum"
-                ];
+                    return [
+                        "view" => VIEW_DIR."home.php",
+                        "meta_description" => "Page d'accueil du forum"
+                    ];
+                }
             }
         }
     }
 
-    public function login() {}
+    public function login() {
+        $userManager = new UserManager();
+
+        if (isset($_POST["submit"])) {
+            $email = filter_input(INPUT_POST,"inputemail", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $password = filter_input(INPUT_POST,"inputPassword1", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+            if($email && $password){
+                if($userManager->findUserByEmail($email) != null){
+                    
+                }
+            }
+        }
+    }
+
     public function logout() {}
 }
