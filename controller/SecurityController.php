7476dbd3306b0dcc987fcfbf128657e800799f80
AHdev67 <antoinehelfer.contact@gmail.com>
2024-04-03 10:53:22 +0200

    added login, logout and profile display methods

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -5,18 +5,20 @@ use App\AbstractController;
 use App\ControllerInterface;
 use App\Session;
 use Model\Managers\UserManager;
+use Model\Managers\TopicManager;
+use Model\Managers\PostManager;
 
 class SecurityController extends AbstractController{
     // contiendra les méthodes liées à l'authentification : register, login et logout
 
-    public function register() {
+    public function displayRegister() {
         return [
             "view" => VIEW_DIR."security/register.php",
             "meta_description" => "User registration form"
         ];
     }
 
-    public function addUser() {
+    public function register() {
         $userManager = new UserManager();
 
         if (isset($_POST["submit"])) {
@@ -72,20 +74,81 @@ class SecurityController extends AbstractController{
         }
     }
 
+    public function displayLogin() {
+        return [
+            "view" => VIEW_DIR."security/login.php",
+            "meta_description" => "User login form"
+        ];
+    }
+
     public function login() {
         $userManager = new UserManager();
 
         if (isset($_POST["submit"])) {
-            $email = filter_input(INPUT_POST,"inputemail", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            $password = filter_input(INPUT_POST,"inputPassword1", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $email = filter_input(INPUT_POST,"inputEmail", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $password = filter_input(INPUT_POST,"inputPassword", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
             if($email && $password){
-                if($userManager->findUserByEmail($email) != null){
+                
+                $checkEmail = $userManager->findUserByEmail($email) ? true : false;
+                
+                if($checkEmail){
+                    // var_dump("test"); die;
+                    $user = $userManager->findUserByEmail($email);
                     
+                    $hash = $user->getPassword();
+                    // var_dump($hash);die;
+                    if(password_verify($password, $hash)){
+                        $_SESSION["user"] = $user;
+                        return [
+                            "view" => VIEW_DIR."home.php",
+                            "meta_description" => "Forum home page"
+                        ];
+                    }
+                    else{
+                        Session::addFlash("error", "Incorrect password");
+                        return [
+                            "view" => VIEW_DIR."security/login.php",
+                            "meta_description" => "User login form"
+                        ];
+                    }
+                }
+                else{
+                    Session::addFlash("error", "Incorrect e-mail");
+                    // $this->redirectTo("security", "displayLogin");
+                    return [
+                        "view" => VIEW_DIR."security/login.php",
+                        "meta_description" => "User login form"
+                    ];
                 }
             }
         }
     }
 
-    public function logout() {}
+    public function logout() {
+        unset($_SESSION["user"]);
+        return [
+            "view" => VIEW_DIR."home.php",
+            "meta_description" => "Forum home page"
+        ];
+    }
+
+    public function profile($id) {
+        $userManager = new UserManager();
+        $postManager = new PostManager();
+        $topicManager = new TopicManager();
+        $user = $userManager->findOneById($id);
+        $topics = $topicManager->findTopicsByUser($id);
+        $posts = $postManager->findPostByUser($id);
+
+        return [
+            "view" => VIEW_DIR."security/displayUser.php",
+            "meta_description" => "List of topics and posts by a user : ".$user,
+            "data" => [
+                "user" => $user,
+                "topics" => $topics,
+                "posts" => $posts
+            ]
+        ];
+    }
 }
