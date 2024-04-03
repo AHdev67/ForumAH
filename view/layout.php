67fdb0fe4815b5187e9eb8fb3849cebfc61b86e0
AHdev67 <antoinehelfer.contact@gmail.com>
2024-04-03 16:59:48 +0200

    corrected redirection

--- view/layout.php
+++ view/layout.php
@@ -26,7 +26,7 @@
                             <?php
                             if(App\Session::isAdmin()){
                                 ?>
-                                <a href="index.php?ctrl=home&action=users">User list</a>
+                                <a href="index.php?ctrl=security&action=users">User list</a>
                             <?php } ?>
                         </div>
                         <div id="nav-right">
