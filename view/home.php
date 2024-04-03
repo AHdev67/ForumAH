0ea6cc433fa10e500a445520fc2bb012305ca720
AHdev67 <antoinehelfer.contact@gmail.com>
2024-04-03 16:59:30 +0200

    started latest topics display

--- view/home.php
+++ view/home.php
@@ -1,7 +1,18 @@
-<h1>BIENVENUE SUR LE FORUM</h1>
+<?php
+    $categories = $result["data"]['categories']; 
+    $topics = $result["data"]['topics']; 
+?>
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
+<h1>The home of PC hardware enthusiasts.</h1>
+<p>Need advice ? Got advice to give ? You're in the right place.</p>
 
 <p>
     <a href="index.php?ctrl=topic&action=displayTopicForm">Start a new topic</a>
 </p>
+
+<h2>Most recent topics</h2>
+
+<?php
+foreach($categories as $category){
+    
+}
