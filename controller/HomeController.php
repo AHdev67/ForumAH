2d870d4bd40becf98cbcadcc63c576c424d4133b
AHdev67 <antoinehelfer.contact@gmail.com>
2024-04-05 12:00:16 +0200

    modify homepage method, now displays 8 latest topics

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -13,17 +13,14 @@ use Model\Managers\UserManager;
 class HomeController extends AbstractController implements ControllerInterface {
 
     public function index(){
-        $categoryManager = new CategoryManager();
         $topicManager = new TopicManager();
 
-        $categories = $categoryManager->findAll(["name", "DESC"]);
-        $topics = $topicManager->findAll(["creationDate", "DESC"]);
+        $topics = $topicManager->findLatestTopics(["creationDate", "DESC"]);
 
         return [
             "view" => VIEW_DIR."home.php",
             "meta_description" => "Page d'accueil du forum",
             "data" => [
-                "categories" => $categories,
                 "topics" => $topics
             ]
         ];
