51328b6f1cd1150033d2b1be09d69fabd46db9dc
AHdev67 <antoinehelfer.contact@gmail.com>
2024-04-05 12:01:43 +0200

    added latest topics display

--- view/home.php
+++ view/home.php
@@ -1,18 +1,23 @@
 <?php
-    $categories = $result["data"]['categories']; 
     $topics = $result["data"]['topics']; 
 ?>
 
 <h1>The home of PC hardware enthusiasts.</h1>
-<p>Need advice ? Got advice to give ? You're in the right place.</p>
+<h2>Need advice ? Got advice to give ? You're in the right place.</h2>
 
 <p>
     <a href="index.php?ctrl=topic&action=displayTopicForm">Start a new topic</a>
 </p>
 
-<h2>Most recent topics</h2>
+<h3>Most recent topics</h3>
 
 <?php
-foreach($categories as $category){
-    
-}
+foreach($topics as $topic){ ?>
+    <div>
+        <p>
+            <a href="index.php?ctrl=topic&action=displayTopic&id=<?= $topic->getId() ?>"><?= $topic->getTitle() ?> </a>
+            by : <?= $topic->getUser() ?> (<?= $topic->getCreationDate() ?>) 
+            Category : <?= $topic->getCategory() ?>
+        </p>
+    </div>
+<?php } ?>
